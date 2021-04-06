ca63fff079f7323501d2db4a0c0cf233fcb0ba7f
juanse132 <juanmanuel0142@hotmail.com>
2021-04-06 16:36:19 -0300

    Hize el frm ventas

---
+++ HuergoMotorsVentas/frmVentas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HuergoMotorsVentas
+{
+    public partial class frmVentas : Form
+    {
+        public frmVentas()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
