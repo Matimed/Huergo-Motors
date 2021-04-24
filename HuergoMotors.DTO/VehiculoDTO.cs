e7b8a03310f8950156c2fb566e44463e14491c9b
Matimed11 <matiascarazomedley@gmail.com>
2021-04-23 21:13:31 -0300

    Hechos los DTO

---
+++ HuergoMotors.DTO/VehiculoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuergoMotors.DTO
+{
+    class VehiculoDTO
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+        public string Modelo { get; set; }
+        public double PrecioVenta { get; set; }
+        public int Stock { get; set; }
+    }
+}
