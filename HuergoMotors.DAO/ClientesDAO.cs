6fe95c99d0d0b8dbaaa94ca6adc5789c730dd9c2
Matimed11 <matiascarazomedley@gmail.com>
2021-06-21 21:22:05 -0300

    Normalizacion de nombres

--- HuergoMotors.DAO/ClientesDAO.cs
+++ HuergoMotors.DAO/ClientesDAO.cs
@@ -5,7 +5,7 @@ namespace HuergoMotors.DAO
 {
     public class ClientesDAO : DAOBase<ClientesDTO> 
     {
-        public void Referenciado(int id)
+        public new void Referenciado(int id)
         {
             if (ReferenciaVentas($"IdCliente = {id}"))
                 throw new Exception("No se puede borrar un cliente que tenga ventas asociadas");
