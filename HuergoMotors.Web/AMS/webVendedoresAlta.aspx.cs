96fabe880ffecd4cbc7c01240f70628505b06f28
c1t1gz <joaquin15poggi@gmail.com>
2021-06-27 19:25:08 -0300

    Arreglo de Vendedores Web

--- HuergoMotors.Web/AMS/webVendedoresAlta.aspx.cs
+++ HuergoMotors.Web/AMS/webVendedoresAlta.aspx.cs
@@ -1,17 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using HuergoMotors.Negocio;
+using HuergoMotors.DTO;
 
 namespace HuergoMotors.Web
 {
-    public partial class VendedoresAlta : System.Web.UI.Page
+    public partial class VendedoresAlta : Page
     {
+        VendedoresNegocio vendedoresNegocio = new VendedoresNegocio();
+        private int Id;
+        private const string Backpage = "../webVendedores.aspx";
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                if (Request.QueryString["id"] != null)
+                {
+                    Id = Convert.ToInt32(Request.QueryString["id"]);
+                }
+                if (!Page.IsPostBack)
+                {
+                    if (Request.QueryString["id"] != null)
+                    {
+                        HelperWeb.LeerDTO(Page.Controls, vendedoresNegocio.BuscarId(Id));
+                        btAceptar.Text = "Guardar Cambios";
+                    }
+                    else
+                    {
+                        btAceptar.Text = "Agregar vehiculo";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lbMsg.Text = ex.Message;
+            }
+        }
+        public void btVolver_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(Backpage);
+        }
 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Request.QueryString["id"] != null)
+            {
+                vendedoresNegocio.ModificarElemento(HelperWeb.GenerarDTO<VendedoresDTO>(Controls, Id));
+            }
+            else
+            {
+                vendedoresNegocio.AgregarElemento(HelperWeb.GenerarDTO<VendedoresDTO>(Controls));
+            }
+            Response.Redirect(Backpage);
         }
     }
 }
--- HuergoMotors.Web/webVendedores.aspx.designer.cs
+++ HuergoMotors.Web/webVendedores.aspx.designer.cs
@@ -87,15 +87,6 @@ namespace HuergoMotors.Web
         protected global::System.Web.UI.WebControls.GridView gvVendedores;
 
         /// <summary>
-        /// Control form1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
         /// Control lbMsg.
         /// </summary>
         /// <remarks>
