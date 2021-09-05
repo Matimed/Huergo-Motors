4cd980f33d07c5dda183714acdc11e0af81e6493
Matimed11 <mcarazomedley@gmail.com>
2021-09-04 23:00:53 -0300

    UserControl Campo Texto y Campo DropDown creados

---
+++ HuergoMotors.Web/UserControls/UserControlCampoDropDown.ascx.designer.cs
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace HuergoMotors.Web
+{
+
+
+    public partial class UserControlCampoDropDown
+    {
+
+        /// <summary>
+        /// Control lbCampo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbCampo;
+
+        /// <summary>
+        /// Control ddlCampo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCampo;
+    }
+}
