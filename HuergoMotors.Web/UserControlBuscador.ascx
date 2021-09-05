611b6f7578f97551dffae7da1b74a495659f35bd
Matimed11 <mcarazomedley@gmail.com>
2021-09-04 21:15:01 -0300

    UserControler Buscador creado

---
+++ HuergoMotors.Web/UserControlBuscador.ascx
@@ -0,0 +1,9 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="UserControlBuscador.ascx.cs" Inherits="HuergoMotors.Web.UserControlBuscador" %>
+
+<div>
+    <asp:Label ID="lbFiltro" runat="server" Text="Filtro"></asp:Label>
+    <asp:TextBox ID="txtFiltro" runat="server" Width="226px"></asp:TextBox>
+    <asp:Button ID="btnBuscar" runat="server" Text="Buscar" ViewStateMode="Disabled" OnClick="btnBuscar_Click" />
+    <asp:Button ID="btnRecargar" runat="server" Text="Recargar" ViewStateMode="Disabled" OnClick="btnRecargar_Click" />
+    <asp:Button ID="btnNuevo" runat="server" Text="Nuevo" ViewStateMode="Disabled" OnClick="btnNuevo_Click" />
+</div>
---
+++ HuergoMotors.Web/UserControlBuscador.ascx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HuergoMotors.Web
+{
+    public partial class UserControlBuscador : System.Web.UI.UserControl
+    {
+
+
+        public event EventHandler BuscarClick;
+        public event EventHandler RecargarClick;
+        public event EventHandler NuevoClick;
+
+        public TextBox Filtro
+        {
+            get { return txtFiltro; }
+        }
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (BuscarClick != null)
+            {
+                BuscarClick(sender, e);
+            }
+        }
+        protected void btnRecargar_Click(object sender, EventArgs e)
+        {
+            if (RecargarClick != null)
+            {
+                RecargarClick(sender, e);
+            }
+        }
+        protected void btnNuevo_Click(object sender, EventArgs e)
+        {
+            if (NuevoClick != null)
+            {
+                NuevoClick(sender, e);
+            }
+        }
+    }
+}
