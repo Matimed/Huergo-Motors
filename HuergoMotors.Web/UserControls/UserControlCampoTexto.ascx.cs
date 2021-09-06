b6c32c66f4d2777a987761a79dd6ae2987d36ef6
Matimed11 <mcarazomedley@gmail.com>
2021-09-05 21:16:57 -0300

    Campo Texto Arreglado

--- HuergoMotors.Web/UserControls/UserControlCampoTexto.ascx.cs
+++ HuergoMotors.Web/UserControls/UserControlCampoTexto.ascx.cs
@@ -9,16 +9,27 @@ namespace HuergoMotors.Web
 {
     public partial class UserControlCampoTexto : System.Web.UI.UserControl
     {
-        
+
+        public string Propiedad
+        {
+            get { return Attributes["Propiedad"]; }
+            set { Attributes["Propiedad"] = value; }
+        }
+
         public string Text
         {
-            set { lbCampo.Text = value; }
+            get { return Attributes["Text"]; }
+            set 
+            {
+                Attributes["Text"] = value;
+                lbCampo.Text = value+":"; 
+            }
         }
         public string Valor
         {
             get { return txtValor.Text; }
             set { txtValor.Text = value; }
         }
-      
+
     }
 }
