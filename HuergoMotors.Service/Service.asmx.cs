b417f7ba8d6256a05f66b26e1d7422340521969e
Matimed11 <mcarazomedley@gmail.com>
2021-10-18 14:13:55 -0300

    Servicio creado

---
+++ HuergoMotors.Service/Service.asmx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace HuergoMotors.Service
+{
+    /// <summary>
+    /// Servicio de ejemplo para Huergo Motors
+    /// </summary>
+    [WebService(Namespace = "http://huergo.edu.ar/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
+    public class Service : System.Web.Services.WebService
+    {
+    }
+}
